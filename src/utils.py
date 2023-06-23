8815a68542345fa048f43ac03da1cbad75e3a0ff
Denis Kuznetsov <kdv522@mail.ru>
2023-06-23 08:01:44 +0300

    5 commit

--- src/utils.py
+++ src/utils.py
@@ -1,13 +1,20 @@
 import json
+"""Импорт библиотеки для работы с json файлами"""
 
 filepath = 'operations.json'
 
 def load_data(filepath):
+    """
+    Загрузка исходных данных
+    """
     with open(filepath, 'r',encoding='utf-8') as file:
         data = json.load(file)
         return data
 
 def executed_operations(data):
+    """
+    Получение исполненных операций
+    """
     state = []
     for i in data:
         if i.get('state') == 'EXECUTED':
@@ -15,16 +22,25 @@ def executed_operations(data):
     return state
 
 def last_operations(data):
+    """
+    Выбор пяти последних операций
+    """
     data.sort(key=lambda x: x.get('date'))
     data.reverse()
     return data[:5]
 
 def account_mask(data):
+    """
+    Маскировка данных счета
+    """
     account_list = data.split(" ")
     numb = account_list[-1]
     return account_list[0] + " " + "**" + " " + numb[-4:]
 
 def card_mask(data):
+    """
+    Маскировка данных карты
+    """
     account_list = data.split(" ")
     if len(account_list) > 2:
         numb = account_list[-1]
@@ -35,6 +51,9 @@ def card_mask(data):
         return account_list[0] + " " + numb[0:4] + " " + numb[4:6] + "** ****" + " " + numb[-4:]
 
 def date(date_str):
+    """
+    Преобразование даты
+    """
     date_str = date_str[:10].split('-')
     new_date_str = date_str[2] + "." + date_str[1] + "." + date_str[0]
     return new_date_str
